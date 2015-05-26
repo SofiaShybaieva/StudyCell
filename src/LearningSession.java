f6d61e0afda584c66f8b11bf350144adfbafa1df
Vic Shybaiev <viacheslavs@opera.com>
2015-05-26 22:39:22 +0300

    Main Menu frame with buttons, Learning session frame with the cell image

--- src/LearningSession.java
+++ src/LearningSession.java
@@ -1,10 +1,50 @@
+import java.awt.Button;
 
-public class LearningSession {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+public class LearningSession extends JFrame{
 
 	LearningImage learningImage;
+	private Image image;
+	private File file;
 	
-	public void  start(){
+	private void loadImage(File file) {
+		try {
+			image = ImageIO.read(file);
+		} catch (IOException e) {
+			System.err.println("Image Cell not found!");
+		}
+	}
+
+	@Override
+	public void paint (Graphics g) {
+		super.paint(g);
+		g.drawImage(image, 0, 0, this);
+	}
+	
+	private void prepareGUI(){
+		this.setLayout(null);
+		this.setSize(1024, 768);
+		this.setTitle("Learning Session");
+		this.setLocationRelativeTo(null);
 		
+		this.setVisible(true);
+		this.file = new File("cell.jpg");
+		loadImage(file);
+		
+		
+	}
+	
+	public void  start() {
+		prepareGUI();
 	}
 	public void  finish(){
 		
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,13 +1,88 @@
+
 import java.awt.*;
 import java.io.File;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
-public class MainMenu extends Frame {
+
+public class MainMenu extends JFrame{
+	private JFrame mainFrame;
 	
-	void MainWindow () {
+	private void prepareGUI(){
+	      
+			this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+			this.setLayout(null);
+			this.setSize(350, 400);
+			this.setTitle("StudyCell");
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+			
+			int x = 75;
+			int y = 20;
+			int w = 200;
+			int h = 60;
+			Button btnStudy=new Button("Обучение");  
+			btnStudy.setBounds(x,y,w,h);  
+			btnStudy.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent evt) {
+					startLearningSession ();
+				}
+			});
+			
+			add(btnStudy);
+
+			Button btnControl=new Button("Тестирование");  
+			btnControl.setBounds(x,y+h*2,w,h);// setting button position  
+			btnControl.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent evt) {
+					System.out.println("Тестирование");
+				}
+			});
+			add(btnControl);  
+
+			Button btnAbout=new Button("О программе");  
+			btnAbout.setBounds(x,y+h*4,w,h);// setting button position  
+			btnAbout.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent evt) {
+					System.out.println("О программе");
+				}
+			});
+			
+			add(btnAbout); 
+
+	}
+	
+	protected void startLearningSession() {
+		LearningSession session = new LearningSession ();
+		session.start();
 		
-//		Button b=new Button("click me");  
+	}
+
+	MainMenu () {
+
+		
+		
+		prepareGUI();
+		
+//		JPanel panel = new JPanel();
+//		panel.setLayout(new FlowLayout());
+//		JLabel label = new JLabel("StudyCell");
+//		panel.add(label);
+//		add(panel);
+//		setSize(300, 300);
+//		setLocationRelativeTo(null);
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setVisible(true);
+//		addWindowListener(new WindowAdapter() {
+//	         public void windowClosing(WindowEvent windowEvent){
+//	            System.exit(0);
+//	         }        
+//	      });    
+
+		//		Button b=new Button("click me");  
 //		b.setBounds(30,100,80,30);// setting button position  
 //		  
 //		add(b);//adding button into frame  
@@ -20,9 +95,9 @@ public class MainMenu extends Frame {
 	
 	public static void main (String args[]) {
 		
-		
-		 
-            	MainMenu m = new MainMenu();
+			
+		 	System.out.println("StudyCell");
+            MainMenu m = new MainMenu();
 		 
 		
 		
